d63192628251d4ba904686cbc1264ee42614c7c2
Hyungjun Kim <hjkim@sollae.co.kr>
2020-04-21 11:43:11 +0900

    2020.4.21 - electron-updater

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
 const path = require("path");
 
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
@@ -39,3 +39,64 @@ app.on("activate", () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+const { autoUpdater } = require("electron-updater");
+
+let updateWin;
+
+function sendStatusToWindow(text) {
+  updateWin.webContents.send("message", text);
+}
+
+function createDefaultUpdaetWindow() {
+  updateWin = new BrowserWindow({
+    backgroundColor: "#eeeeee",
+    webPreferences: { nodeIntegration: true },
+  });
+
+  updateWin.on("closed", () => {
+    updateWin = null;
+  });
+  updateWin.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  return updateWin;
+}
+
+autoUpdater.on("checking-for-update", () => {
+  sendStatusToWindow("Checking for update...");
+});
+autoUpdater.on("update-available", (info) => {
+  sendStatusToWindow("Update available.");
+});
+autoUpdater.on("update-not-available", (info) => {
+  sendStatusToWindow("Update not available.");
+});
+autoUpdater.on("error", (err) => {
+  sendStatusToWindow("Error in auto-updater. " + err);
+});
+autoUpdater.on("download-progress", (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + " - Downloaded " + progressObj.percent + "%";
+  log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
+  sendStatusToWindow(log_message);
+});
+autoUpdater.on("update-downloaded", async (info) => {
+  sendStatusToWindow("Update downloaded");
+
+  const option = {
+    type: "question",
+    buttons: ["업데이트", "취소"],
+    defaultId: 0,
+    title: "electron-updater",
+    message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
+  };
+  let btnIndex = await dialog.showMessageBox(updateWin, option);
+
+  if (btnIndex === 0) {
+    autoUpdater.quitAndInstall();
+  }
+});
+
+app.on("ready", async () => {
+  createDefaultUpdaetWindow();
+  autoUpdater.checkForUpdates();
+});
